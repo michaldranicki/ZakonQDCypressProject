28c9b1ca1f2faff3c49e3905f4f97b8a6e99e507
michaldranicki <dranickimeister@gmail.com>
2021-01-11 22:16:31 +0100

    adding more locators and elements for home page

--- cypress/pageobjects/pageelements/HomePageElement.ts
+++ cypress/pageobjects/pageelements/HomePageElement.ts
@@ -13,7 +13,43 @@ export default class HomePageElements{
 
     signInButton(){
 
-        cy.get(locator.homePage.signIn)
+        return cy.get(locator.homePage.signIn)
+
+    }
+
+    popularSection(){
+
+        return cy.get(locator.homePage.popularSection)
+
+    }
+
+    bestSellerSection(){
+
+        return cy.get(locator.homePage.bestSellerSection)
+
+    }
+
+    clothesArea(){
+
+        return cy.get(locator.homePage.clothesArea)
+
+    }
+
+    womenSection(){
+
+        return cy.get(locator.homePage.womenSection)
+
+    }
+
+    dressesSection(){
+
+        return cy.get(locator.homePage.dressesSection)
+
+    }
+
+    tshirtSection(){
+
+        return cy.get(locator.homePage.tshirtSection)
 
     }
 
