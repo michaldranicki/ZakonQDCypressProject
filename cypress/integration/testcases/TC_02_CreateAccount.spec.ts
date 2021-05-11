cd8ad0c545540adc91859e15d27989d4b2c96a67
michaldranicki <dranickimeister@gmail.com>
2021-05-11 18:05:50 +0200

    button

--- cypress/integration/testcases/TC_02_CreateAccount.spec.ts
+++ cypress/integration/testcases/TC_02_CreateAccount.spec.ts
@@ -4,25 +4,20 @@
 
 import {navigateToUrl} from "../../pageobjects/pageactions/HomePageAction";
 import {isHomePage} from "../../pageobjects/pageassertions/HomePageAssertion";
+import {signInButton} from "../../pageobjects/pageelements/HomePageElement";
 
 describe("Create account",()=>{
 
         before(()=>{
-    
            navigateToUrl()
-    
         })
     
         it("Validate if it is homePage",()=>{
-    
         isHomePage()
-    
         })
     
         it("Access sign in page",()=>{
-
-
-
+        signInButton().click()
         })
     
     
